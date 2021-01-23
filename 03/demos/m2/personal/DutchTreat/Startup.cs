996b57cead53b598c3dc0c61926b6e6c0bd47363
Alex Fuller <fulleralex47@gmail.com>
2021-01-23 09:39:07 -0500

    Bare bones DutchTreat app build

---
+++ 03/demos/m2/personal/DutchTreat/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //If the page is run as a dev, will rebuild custom
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //will build routing 
+            //app.UseRouting();
+
+            //will build endpoints
+            //app.UseEndpoints(endpoints =>
+            //{
+                app.Run(async context =>//basically turn off URL routing, all pages load single hello world
+                {
+                    await context.Response.WriteAsync("<html><body><h1>Hello World!</h1></body></html>");
+                });
+            //});
+        }
+    }
+}
