ed61d1c0fad5dcab524942983c2538d8bc80a66f
Alex Fuller <fulleralex47@gmail.com>
2021-01-26 07:14:25 -0500

    Added a shop page with the context in the app controller. Once the connection between the context and the AppController is established, the server can begin to be queried using LINQ.

--- 07/demos/m6/personal/DutchTreat/DutchTreat/Controllers/AppController.cs
+++ 07/demos/m6/personal/DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -3,52 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using DutchTreat.Services;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DutchTreat.Controllers
 {
-  public class AppController : Controller
-  {
-    private readonly IMailService _mailService;
-
-    public AppController(IMailService mailService)
+    public class AppController : Controller
     {
-      _mailService = mailService;
+        private readonly IMailService _mailService;
+        private readonly DutchContext _context;
+
+        //Bring in the context which is importing the data from the server
+        public AppController(IMailService mailService, DutchContext context)
+        {
+            _mailService = mailService;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send the email
+                _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+
+        //In the reference to the shop page, references to the DB can now be queried with LINQ
+        public IActionResult Shop()
+        {
+            var results = _context.Products
+                .OrderBy(p => p.Category)
+                .ToList(); //Get all products from the Db ordered by category
+
+            return View(results);
+        }
+
     }
-
-    public IActionResult Index()
-    {
-      return View();
-    }
-
-    [HttpGet("contact")]
-    public IActionResult Contact()
-    {
-      return View();
-    }
-
-    [HttpPost("contact")]
-    public IActionResult Contact(ContactViewModel model)
-    {
-      if (ModelState.IsValid)
-      {
-        // Send the email
-        _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
-        ViewBag.UserMessage = "Mail Sent";
-        ModelState.Clear();
-      }
-
-      return View();
-    }
-
-    public IActionResult About()
-    {
-      ViewBag.Title = "About Us";
-
-      return View();
-    }
-
-  }
 }
