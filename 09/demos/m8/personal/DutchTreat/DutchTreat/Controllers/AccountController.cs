34ce00b04acfbbfbe507319568805360c3cbd929
Alex Fuller <fulleralex47@gmail.com>
2021-02-02 08:17:48 -0500

    Implimented Login and Logout Logic

--- 09/demos/m8/personal/DutchTreat/DutchTreat/Controllers/AccountController.cs
+++ 09/demos/m8/personal/DutchTreat/DutchTreat/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,10 +14,12 @@ namespace DutchTreat.Controllers
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> logger;
+        private readonly SignInManager<StoreUser> signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
         {
             this.logger = logger;
+            this.signInManager = signInManager;
         }
 
         public IActionResult Login()
@@ -25,6 +30,45 @@ namespace DutchTreat.Controllers
             }
 
             return View();
+        }
+
+        //Login
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Confirm Login
+                var result = await signInManager.PasswordSignInAsync
+                    (
+                        model.Username, 
+                        model.Password, 
+                        model.RememberMe, 
+                        false
+                    );
+                //If Login sucessful, either redirect to the url requested or default to shop page
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    else
+                       return RedirectToAction("Shop", "App");                    
+                }
+            }
+
+            //Login failed generic
+            ModelState.AddModelError("", "Failed to login");
+
+            //postback to login page on failed login
+            return View();
+        }
+
+        //Logout
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await this.signInManager.SignOutAsync();
+            return RedirectToAction("Index", "App");
         }
     }
 }
