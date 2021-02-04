d057f07dbd3f28100c4f17a51995bd4b43b6c8d2
Alex Fuller <fulleralex47@gmail.com>
2021-02-04 05:48:04 -0500

    Added basic typescript

---
+++ 10/demos/m9/personal/DutchTreat/DutchTreat/wwwroot/ts/customerstore.ts
@@ -0,0 +1,34 @@
+﻿// The export keyword stops this class from being global and requires importing where needed
+// export class StoreCustomer {
+class StoreCustomer {
+
+    // property types are not required, you can let the interpreter infer it or explicitly pass it with ":"
+    public visits: number = 0;
+    private ourName: string;
+
+    // Constructors require the "constructor" keyword but otherwise follow the function format
+    // You can declare private properties in the params of the constructor
+    // that will automatically make them available to the class
+    constructor(private firstName: string, private lastName: string) {
+    }
+
+    // properties need to be declared then explicitly referred to using "this."
+    set name(value) {
+        this.ourName = value;
+    }
+    get name() {
+        return this.ourName;
+    }
+
+    // Function does not explicitly need to be called a function, interpreted by the signature
+    // public showName(name: string): boolean {
+    //    alert(name);
+    //    return true;
+    // }
+    public showName() {
+        alert(this.firstName + " " + this.lastName); // ctor properties are wired
+    }
+}
+
+// let cust = new StoreCustomer();
+// cust.visits = "not a number"; //Example of type safety
---
+++ 10/demos/m9/personal/DutchTreat/DutchTreat/wwwroot/ts/main.ts
@@ -0,0 +1,6 @@
+﻿// Explicitly can tell what classes to pull from a file
+// import { StoreCustomer } from "./customerstore";
+
+// let is a reassignable var, const is a constant
+let shopper = new StoreCustomer("Alex", "Fuller");
+shopper.showName();
